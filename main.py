68206a127c6bdd8e24352dbc727f9d17acbf7c5a
fluff3rbot <76801259+fluff3rbot@users.noreply.github.com>
2021-02-08 20:59:29 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import asyncio
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------')
+
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+
+
+client.run(os.environ['token'])
