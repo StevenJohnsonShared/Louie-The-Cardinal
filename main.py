319b9e1ffbe555e79ccb58b2e83b6c95f0efcf4f
fluff3rbot <76801259+fluff3rbot@users.noreply.github.com>
2021-03-01 19:41:23 -0500

    Update main.py
    updating the command prefix as to not conflict with other bots

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="-")
 
 @client.event
 async def on_ready():
